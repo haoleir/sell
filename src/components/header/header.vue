f34f3efc6e8cac577b8ed25044ec4040188495c0
haoleir <haoleir@gmail.com>
2021-01-24 14:15:46 +0800

    update seller

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,18 +17,21 @@
                  <span class="text">{{seller.supports[0].description}}</span>
              </div>
          </div>
-         <div v-if="seller.supports" class="support-count">
+         <div v-if="seller.supports" class="support-count" @click="showDetail">
              <span class="count">{{seller.supports.length}}个</span>
              <i class="icon-keyboard_arrow_right"></i>
          </div>
      </div>
-     <div class="bulletin-wrapper">
+     <div class="bulletin-wrapper" @click="showDetail">
          <span class="bulletin-title"></span>
          <span class="bulletin-text">{{seller.bulletin}}</span>
          <i class="icon-keyboard_arrow_right"></i>
      </div>
      <div class="background">
          <img :src="seller.avatar" width="100%" height="100%">
+     </div>
+     <div v-show="detailShow" class="detail">
+
      </div>
     </div>
 </template>
@@ -42,8 +45,18 @@ export default {
       }
     }
   },
+  data() {
+    return {
+      detailShow: false
+    };
+  },
   created() {
     this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+  },
+  methods: {
+    showDetail() {
+      this.detailShow = true;
+    }
   }
 };
 </script>
@@ -53,6 +66,7 @@ export default {
 
 .header {
     position: relative;
+    overflow: hidden;
     color: #ffffff;
     background: rgba(7, 17, 27, 0.5);
 
@@ -176,7 +190,7 @@ export default {
         .bulletin-title {
             display: inline-block;
             vertical-align: top;
-            margin-top: 7px;
+            margin-top: 8px;
             width: 22px;
             height: 12px;
             bg-image('bulletin');
@@ -206,6 +220,17 @@ export default {
         height: 100%;
         z-index: -1;
         filter: blur(10px);
+    }
+
+    .detail {
+        position: fixed;
+        z-index: 100;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+        background: rgba(7, 17, 27, 0.8);
     }
 }
 </style>        
