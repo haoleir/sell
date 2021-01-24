857ee4b7926f965d8200ffa4a2294717b52cd4e3
haoleir <haoleir@gmail.com>
2021-01-24 18:12:00 +0800

    complete header

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,19 +7,19 @@ module.exports = {
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: 'standard',
   // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
+  plugins: ['html'],
   // add your custom rules here
-  'rules': {
+  rules: {
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'semi': ['error', 'always'],
-    'indent':0,
-    'space-before-function-paren':0
+    semi: ['error', 'always'],
+    indent: 0,
+    'space-before-function-paren': 0,
+    'no-unmodified-loop-condition': 0
+    // ,'no-unused-vars': 0
   }
-}
+};
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -30,17 +30,43 @@
      <div class="background">
          <img :src="seller.avatar" width="100%" height="100%">
      </div>
-     <div v-show="detailShow" class="detail">
+     <div v-show="detailShow" class="detail" transition="fade">
          <div class="detail-wrapper clearfix">
-             <div class="detail-main"></div>
+             <div class="detail-main">
+                 <h1 class="name">{{seller.name}}</h1>
+                 <div class="star-wrapper">
+                    <Star :size="48" :score="seller.score"></Star>
+                 </div>
+                 <div class="title">
+                     <div class="line"></div>
+                     <div class="text">优惠信息</div>
+                     <div class="line"></div>
+                 </div>
+                 <ul v-if="seller.supports" class="supports">
+                    <li v-for="(index, item) in seller.supports" :key="index" class="support-item">
+                        <span class="icon" :class="classMap[item.type]"></span>
+                        <span class="text">{{item.description}}</span>
+                    </li>
+                 </ul>
+                 <div class="title">
+                     <div class="line"></div>
+                     <div class="text">商家公告</div>
+                     <div class="line"></div>
+                 </div>
+                 <div class="bulletin">
+                     <div class="content">{{seller.bulletin}}</div>
+                 </div>
+             </div>
          </div>
-         <div class="detail-close">
+         <div class="detail-close" @click="hideDetail">
              <i class="icon-close"></i>
          </div>
      </div>
     </div>
 </template>
 <script type="text/ecmascript-6">
+import Star from 'components/star/star.vue';
+
 export default {
   props: {
     seller: {
@@ -50,6 +76,7 @@ export default {
       }
     }
   },
+  components: { Star },
   data() {
     return {
       detailShow: false
@@ -61,6 +88,9 @@ export default {
   methods: {
     showDetail() {
       this.detailShow = true;
+    },
+    hideDetail() {
+      this.detailShow = false;
     }
   }
 };
@@ -235,14 +265,119 @@ export default {
         width: 100%;
         height: 100%;
         overflow: auto;
-        background: rgba(7, 17, 27, 0.8);
+        transition: all 0.5s;
+        backdrop-filter: blur(10px);
+
+        &.fade-transition {
+            opacity: 1;
+            background: rgba(7, 17, 27, 0.8);
+        }
+
+        &.fade-enter, &.fade-leave {
+            opacity: 0;
+            background: rgba(7, 17, 27, 0);
+        }
 
         .detail-wrapper {
             min-height: 100%;
+            width: 100%;
 
             .detail-main {
                 margin-top: 64px;
                 padding-bottom: 64px;
+
+                .name {
+                    line-height: 16px;
+                    text-align: center;
+                    font-size: 16px;
+                    font-weight: 700;
+                }
+
+                .star-wrapper {
+                    margin-top: 18px;
+                    padding: 2px 0;
+                    text-align: center;
+                }
+
+                .title {
+                    display: flex;
+                    width: 80%;
+                    margin: 28px auto 24px auto;
+
+                    .line {
+                        flex: 1;
+                        position: relative;
+                        top: -6px;
+                        border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+                    }
+
+                    .text {
+                        padding: 0 12px;
+                        font-size: 14px;
+                        font-weight: 700;
+                    }
+                }
+
+                .supports {
+                    width: 80%;
+                    margin: 0 auto;
+
+                    .support-item {
+                        padding: 0 12px;
+                        margin-bottom: 12px;
+                        font-size: 0;
+
+                        &.last-child {
+                            margin-bottom: 0;
+                        }
+
+                        .icon {
+                            display: inline-block;
+                            width: 16px;
+                            height: 16px;
+                            vertical-align: top;
+                            margin-right: 6px;
+                            background-size: 16px 16px;
+                            background-repeat: no-repeat;
+
+                            &.decrease {
+                                bg-image('decrease_2');
+                            }
+
+                            &.discount {
+                                bg-image('discount_2');
+                            }
+
+                            &.guarantee {
+                                bg-image('guarantee_2');
+                            }
+
+                            &.invoice {
+                                bg-image('invoice_2');
+                            }
+
+                            &.special {
+                                bg-image('special_2');
+                            }
+                        }
+
+                        .text {
+                            line-height: 16px;
+                            font-size: 12px;
+                        }
+                    }
+                }
+
+                .bulletin {
+                    width: 80%;
+                    margin: 0 auto;
+
+                    .content {
+                        padding: 0 12px;
+                        line-height: 24px;
+                        font-size: 12px;
+                    }
+                }
             }
         }
 
